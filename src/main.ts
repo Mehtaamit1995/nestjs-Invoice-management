8cdcf4849cea6602814011f7be7cd84a219a6f82
Amit Mehta <JohnWickedStine@Kunals-MacBook-Air.local>
2021-08-25 18:46:28 +0530

    second commit

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT;
-  await app.listen(port);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
